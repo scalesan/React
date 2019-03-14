02352d5f50ad0f7b1bd66fc321b79bc06d6bed57
Anthony Scales <ascales@nexient.com>
2019-03-14 16:09:44 -0400

    More Redux

--- React/Redux/blog/src/components/PostList.js
+++ React/Redux/blog/src/components/PostList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect } from 'react-redux';
-import { fetchPosts } from '../actions'
+import { fetchPosts } from '../actions';
+import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
     componentDidMount(){
@@ -11,12 +12,13 @@ class PostList extends React.Component {
         return this.props.posts.map(post => {
             return (
                 <div className ="item" key={post.id}>
-                    <i className =" large middle aligned icon user" />
+                    <i className ="large middle aligned icon user" />
                     <div className ="content">
                         <div className ="description">
                             <h2>{post.title}</h2>
                             <p>{post.body}</p>
                         </div>
+                        <UserHeader userId = {post.userId}/>
                     </div>
                 </div>
             )
