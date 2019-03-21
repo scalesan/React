3f9574a176d184313e838e856e1ab3f7b410de59
Anthony Scales <ascales@nexient.com>
2019-03-21 16:48:37 -0400

    More Redux

--- React/Redux/streams/src/components/streams/StreamCreate.js
+++ React/Redux/streams/src/components/streams/StreamCreate.js
@@ -1,7 +1,67 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>
-};
+class StreamCreate extends React.Component {
+    renderError({error, touched}) {
+        if(touched && error){
+            return (
+                <div className = "ui error message">
+                    <div className = "header">{error}</div>
+                </div>
+            )   
+        }
+    }    
 
-export default StreamCreate;
+    renderInput = ({input, label, meta }) => {
+        const className = `field ${meta.error && meta.touched ? 'error': ''}`;
+        return (
+            <div className ={className}>
+            <label>{label}</label>
+            <input {...input} autoComplete="off" />
+            {this.renderError(meta)}
+            </div>
+        )
+    }
+
+    onSubmit = (formValues) => {
+        this.props.createStream(formValues)
+    }
+
+    render() {
+        return (
+            <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+                <Field name="title" 
+                component={this.renderInput} 
+                label="Enter title"
+                />
+                <Field name="description" 
+                component={this.renderInput}
+                label="Enter Description" 
+                />
+                <button className = "ui button primary">Submit</button>
+            </form>
+        )
+    }
+}
+
+const validate = (formValues) => {
+    const errors = {};
+    if(!formValues.title){
+        errors.title = "Title Required!!!";
+    }
+
+    if(!formValues.description){
+        errors.description = "Description Required!!!";
+    }
+
+    return errors;
+}
+
+ const formWrapped = reduxForm({
+    form: 'streamCreate',
+    validate: validate
+})(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped)
